3a303e21d82be22cee817382e75e671d43e792b2
mpenning <mike@pennington.net>
2022-01-16 13:05:05 -0600

    Relicense BSD 3-Clause, improve Makefile, Migrate to poetry

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+VERSION := $(shell grep version pyproject.toml | sed -r 's/^version\s*=\s*"(\S+?)"/\1/g')
+.PHONY: pylama
+pylama:
+	pylama --ignore=E501,E301,E265,E266 polymer/*.py | less -XR
 .PHONY: clean
 clean:
 	find ./* -name '*.pyc' -exec rm {} \;
@@ -10,9 +14,40 @@ clean:
 	-rm -rf .cache/
 	-rm -rf build/ dist/ polymer.egg-info/ setuptools*
 
+.PHONY: repo-push
+repo-push:
+	git remote remove origin
+	git remote add origin "git@github.com:mpenning/polymer"
+	git push git@github.com:mpenning/polymer.git
+	git push origin +master
+
+.PHONY: repo-push-tag
+repo-push-tag:
+	git remote remove origin
+	git remote add origin "git@github.com:mpenning/polymer"
+	git tag -a ${VERSION} -m "Tag with ${VERSION}"
+	git push git@github.com:mpenning/polymer.git
+	git push origin +master
+	git push --tags origin ${VERSION}
+
+.PHONY: repo-push-force
+repo-push-force:
+	git remote remove origin
+	git remote add origin "git@github.com:mpenning/polymer"
+	git push git@github.com:mpenning/polymer.git
+	git push --force-with-lease origin +master
+
+.PHONY: repo-push-tag-force
+repo-push-tag-force:
+	git remote remove origin
+	git remote add origin "git@github.com:mpenning/polymer"
+	git tag -a ${VERSION} -m "Tag with ${VERSION}"
+	git push git@github.com:mpenning/polymer.git
+	git push --force-with-lease origin +master
+	git push --force-with-lease --tags origin ${VERSION}
+
 .PHONY: pypi
 pypi:
-	-make clean
-	-python setup.py register -r pypi
-	-python setup.py bdist_wheel sdist
-	-twine upload dist/*
+	make clean
+	poetry build
+	poetry publish
--- polymer/Polymer.py
+++ polymer/Polymer.py
@@ -14,39 +14,46 @@ import time
 import sys
 import os
 
-if sys.version_info < (3, 0):
-    import cPickle as pickle  # Python2
-else:
-    import _pickle as pickle  # Python3
+import _pickle as pickle  # Python3
 
-try:
-    # This works in Python2.7...
-    from Queue import Empty, Full
-except ImportError:
-    # This works in Python3.x...
-    from queue import Empty, Full
+# This works in Python3.x...
+from queue import Empty, Full
 
 from colorama import init as color_init
 from colorama import Fore, Style
 
 """ Polymer.py - Manage parallel tasks
-     Copyright (C) 2015-2020 David Michael Pennington
+     Copyright (C) 2021-2022 David Michael Pennington
+     Copyright (C) 2020-2021 David Michael Pennington at Cisco Systems
+     Copyright (C) 2019      David Michael Pennington at ThousandEyes
+     Copyright (C) 2015-2019 David Michael Pennington at Samsung Data Services
 
-     This program is free software: you can redistribute it and/or modify
-     it under the terms of the GNU General Public License as published by
-     the Free Software Foundation, either version 3 of the License, or
-     (at your option) any later version.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
 
-     This program is distributed in the hope that it will be useful,
-     but WITHOUT ANY WARRANTY; without even the implied warranty of
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     GNU General Public License for more details.
+1.  Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
 
-     You should have received a copy of the GNU General Public License
-     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+2.  Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
 
-     If you need to contact the author, you can do so by emailing:
-     mike [~at~] pennington [/dot\] net
+3.  Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 class SharedCounter(object):
@@ -57,6 +64,7 @@ class SharedCounter(object):
     second process may read the old value before the new one is written by the
     first process. The solution is to use a multiprocessing.Lock to guarantee
     the atomicity of the modifications to Value.
+
     This class comes almost entirely from Eli Bendersky's blog:
     http://eli.thegreenplace.net/2012/01/04/shared-counter-with-pythons-multiprocessing/
     """
@@ -166,7 +174,7 @@ class Worker(object):
 
         try:
             self.message_loop(todo_q, done_q)  # do work
-        except:
+        except Exception:
             if self.task is not None:
                 self.task.task_stop = time.time()  # Seconds since epoch
 
@@ -344,7 +352,7 @@ class Worker(object):
             except Full:
                 time.sleep(0.1)
             ## Disable extraneous error handling...
-            except:
+            except Exception:
                 if self.task is not None:
                     self.task.task_stop = time.time()  # Seconds since epoch
                 # Handle all other errors here...
@@ -606,7 +614,7 @@ class TaskMgr(object):
                             try:
                                 # Delete the worker assignment...
                                 self.worker_assignments.pop(w_id)
-                            except:
+                            except Exception:
                                 pass
                             self.retval.add(task)  # Add result to retval
 
