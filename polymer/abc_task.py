5d717faae65249b6783067de24397308ce4ff1e5
mpenning <mike@pennington.net>
2023-03-05 09:07:39 -0600

    Cosmetic code change

--- polymer/abc_task.py
+++ polymer/abc_task.py
@@ -15,8 +15,7 @@ class BaseTask(object):
         self.task_stop = 0.0  # Data timestamp, float seconds since epoch
         self.task_start = 0.0  # When task was run by a worker
         self.queue_time = 0.0  # When TaskMgr queued task for a worker
-
-        self.worker_loop_delay = 0.00001  # Default 10us worker sleep delay
+        self.worker_loop_delay = 0.00001  # Default 10us worker sleep loop_delay
 
 
     def __hash__(self):
